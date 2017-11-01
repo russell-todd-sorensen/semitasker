68c74c06564b1c2f0e49b47b50d26ce33c1eff67
Russell Sorensen <russell.todd.sorensen@gmail.com>
2017-10-31 23:05:08 -0700

    Added small footprint version of Mouthful

---
+++ chemical-pi/js/mouthful-search.js
@@ -0,0 +1,29 @@
+// mouthful-search.js
+
+function getElement(attr,value) {
+  var chemicalData = new Object();
+  for (var i = 0;i < chemicalPiData.length ;i++) {
+    chemicalData = chemicalPiData[i];
+    if (chemicalData === undefined) {
+      chemicalData =  chemicalPiData[chemicalPiData.length-1];
+    }
+    if (chemicalData[attr] == value) {
+      break;
+    }
+  }
+  return chemicalData;
+}
+
+function getMouthful(atomicNumber) {
+  var mouthful = '';
+  var value = '';
+  var index = parseInt(atomicNumber);
+  for (var i = 10*index; i< (10*index+10); i++) {
+    value = piDigits[i];
+    if (value === undefined) {
+      return ' 3 ';
+    }
+    mouthful += value  + ' ';
+  }
+  return mouthful;
+}
