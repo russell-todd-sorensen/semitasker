8d2bcae90c3c1ac7e0e645952f4e40eb3f480619
Russell Sorensen <russell.todd.sorensen@gmail.com>
2018-12-15 15:38:00 -0800

    Fixed label tool so it is more flexable. Ready to generalize

--- hom/labels/js/folder-labels-with-color.js
+++ hom/labels/js/folder-labels-with-color.js
@@ -39,25 +39,25 @@ var labelData = {
 
 
 // zero indexed row and column helpers:
-function getRow (index) {
+var getRow = function(index) {
 	return Math.floor(index/2);
 }
-function getCol (index) {
+var getCol = function(index) {
 	return index % 2;
 }
 
 
 // generic base+step*index+skew calculation
-function getItemX (item, index) {
+var getItemX = function(item, index) {
 	return labelData[item].x + getCol(index) * labelData[item].dx + skewX;
 }
 
-function getItemY (item, index) {
+var getItemY = function(item, index) {
 	return labelData[item].y + getRow(index) * labelData[item].dy + skewY;
 }
 
 
-function deleteLabel (index,svgId) {
+var deleteLabel = function(index,svgId) {
 	var svg = d3.select('#' + svgId);
 	var template = '#label-' + index;
 	svg.select(template).remove();
@@ -71,12 +71,14 @@ function deleteLabel (index,svgId) {
 	head.select(template + "-css").remove();
 }
 // Generate One Label
-function generateLabel (index,svgId,data) {
+var generateLabel = function(index,svgId,data) {
 
 	var svg = d3.select('#' + svgId);
 	var rect = svg.append('rect');
 	var head = d3.select('head');
 	var labelId = 'label-' + index;
+
+	// note Not sure onclick handler is necessary
 	rect
 	 .attr('id',labelId)
 	 .attr('class','label8366')
@@ -87,11 +89,18 @@ function generateLabel (index,svgId,data) {
 	 	  var re = /(label-)([0-9]*)/;
 	 	  var match =  data.match(re);
 	 	  var labelId = match[0];
-	 	  $('#' + formIds.editIndex).val(match[2]);
+		  var index = match[2];
 
-	 	  getLabelData(labelId,formIds.docid);
-	 	  getLabelData(labelId,formIds.pname);
+	 	  $('#' + formIds.editIndex).val(index);
+
+		  setLabelData(index, labelId, formIds.docid);
+		  setLabelData(index, labelId, formIds.pname);
+		  setLabelSelectData(index, labelId, formIds.house);
+		  setLabelSelectData(index, labelId, formIds.role);
+		  setLabelSelectData(index, labelId, formIds.extra);
 	 });
+
+	bindLabelClick(index, '#' + labelId);
 
 	 if (data.house == null &&
 		data.pname == null &&
@@ -149,7 +158,7 @@ function generateLabel (index,svgId,data) {
 }
 
 // Generate all labels from given starting point
-function createLabels(startIndex, svgId, labelDataArray) {
+var createLabels = function(startIndex, svgId, labelDataArray) {
 
 	var count = labelDataArray.length;
 	for (var i = startIndex, j = 0; i < count+startIndex; i++, j++) {
@@ -157,9 +166,130 @@ function createLabels(startIndex, svgId, labelDataArray) {
 	}
 }
 
-function destroyLabels(startIndex,svgId,labelDataArray) {
+var destroyLabels = function(startIndex,svgId,labelDataArray) {
 	var count = labelDataArray.length;
 	for (var i = startIndex, j = 0; i < count+startIndex; i++, j++) {
 		deleteLabel(i,'svg');
 	}
 }
+
+
+var getLabelData = function(labelId, field) {
+	var val = $('#' + labelId + "-" + formIds[field]).text();
+	$('#' + formIds[field]).val(val);
+	Log.Notice(field + '="' + val + '"');
+	return val
+}
+
+var setLabelData = function(labelIndex, labelId, field) {
+	var val = Labels.participants[labelIndex][field]
+	$('#' + formIds[field]).val(val);
+	Log.Notice(field + '="' + val + '"');
+	return val
+}
+
+var getLabelSelectData = function(labelId, field) {
+	var val = $('#' + labelId + "-" + formIds[field]).text();
+	Log.Notice(field + '="' + val + '"');
+
+	var selectedOption = "#" + formIds[field] + " " + "option:selected";
+    $(selectedOption).removeAttr('selected');
+    var valueOption = "#" + formIds[field] + " " + "option[value='" + val + "']";
+    $(valueOption).attr('selected', 'selected');
+	return val
+}
+
+var setLabelSelectData = function(labelIndex, labelId, field) {
+	var val = Labels.participants[labelIndex][field]
+	Log.Notice(field + '="' + val + '"');
+
+	var selectedOption = "#" + formIds[field] + " " + "option:selected";
+    $(selectedOption).removeAttr('selected');
+    var valueOption = "#" + formIds[field] + " " + "option[value='" + val + "']";
+    $(valueOption).attr('selected', 'selected');
+	return val
+}
+
+var setEditLabelIndex = function( evt ) {
+
+	var data = this.id;
+	var re = /(label-)([0-9]*)/;
+	var match =  data.match(re);
+	var labelId = match[0];
+	var index = evt.data;
+
+	$('#' + formIds.editIndex).val(match[2]);
+
+	setLabelData(index, labelId, formIds.docid);
+	setLabelData(index, labelId, formIds.pname);
+	setLabelSelectData(index, labelId, formIds.house);
+	setLabelSelectData(index, labelId, formIds.role);
+	setLabelSelectData(index, labelId, formIds.extra);
+}
+
+var restoreAll = function(inputId) {
+	var val = $('#' + inputId).val();
+	Labels.participants = JSON.parse(val);
+	Log.Notice("restoreAll('" + inputId + "')");
+}
+
+var updateLabel = function() {
+
+	var id = parseInt($('#' + formIds.editIndex).val());
+	var pname = $('#' + formIds.pname).val();
+	var docid = $('#' + formIds.docid).val();
+	var house = $('#' + formIds.house + " option:selected").val();
+	var role = $('#' + formIds.role + " option:selected").val();
+    var extra = $('#' + formIds.extra + " option:selected").val();
+
+	Labels.participants[id] = {
+		pname:pname,
+		docid:docid,
+		house:house,
+		role:role,
+		extra:extra
+	};
+
+	reloadLabels();
+}
+
+var reloadLabels = function() {
+
+    saveAll();
+	destroyLabels(0,'svg',Labels.participants);
+	createLabels(0,'svg',Labels.participants);
+};
+
+var clearLabel = function () {
+	var id = parseInt($('#' + formIds.editIndex).val());
+	Labels.participants[id] = {
+		pname:null,
+		docid:null,
+		house:null,
+		role:null,
+		extra:null
+	};
+	reloadLabels();
+};
+
+var saveAll = function() {
+	var id = formIds.data
+	var val = JSON.stringify(Labels.participants);
+	$('#' + id).val(val);
+	Data.saveInput(id,'restoreAll');
+	Log.Notice('saved all');
+};
+
+var bindLabelClicks = function (selector) {
+
+    var labels = $(selector);
+    var node;
+    for (i=0; i< labels.length;i++) {
+      	node = labels[i].id
+      	$('#' + node).bind('click',i,setEditLabelIndex);
+    }
+};
+
+var bindLabelClick = function (index,selector) {
+    $(selector).bind('click',index,setEditLabelIndex);
+};
