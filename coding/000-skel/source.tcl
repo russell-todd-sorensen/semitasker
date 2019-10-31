a04b0634f0f0827514c681d0af64d30b36c06716
Russell Sorensen <russell.todd.sorensen@gmail.com>
2019-10-31 21:53:45 +0000

    Updated template for problem solver code

--- coding/000-skel/source.tcl
+++ coding/000-skel/source.tcl
@@ -1,8 +1,10 @@
 set urlList [lrange [ns_conn urlv] 0 end-1]
 ns_log Notice "urlList=$urlList"
-set file [ns_conn query]
-if {$file == "" || [string first / $file] > -1} {
+set fileNum [ns_conn query]
+if {$fileNum == "" || ![string is integer -strict $fileNum]} {
     set file code.tcl
+} else {
+    set file code-$fileNum.tcl
 }
 
 lappend urlList $file
