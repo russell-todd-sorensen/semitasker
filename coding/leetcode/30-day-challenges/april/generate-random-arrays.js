e778a96f0e6b7ed1a5ab11349630417d77970a73
Russell Sorensen <russell-github-commits@highfivediet.com>
2021-12-01 18:19:23 -0800

    Updated random array generation code eliminate O(N) ops
    Eliminated pop,push,splice etc. New code generates one set
    of random numbers within range then adds duplicate array
    to end of this with random swap operation.

--- coding/leetcode/30-day-challenges/april/generate-random-arrays.js
+++ coding/leetcode/30-day-challenges/april/generate-random-arrays.js
@@ -4,6 +4,25 @@ var tenMillPlus = function (repeat,arr) {
     return new Array(repeat).fill(arr).flat();
 }
 
+function genIntegerList(len,min,max) {
+    len = len?len:10;
+    min = min?min:0;
+    max = max?max:2**30;
+
+    let range = max-min,
+        list  = [];
+
+    // Min Range value check
+    if (range < Number.EPSILON) {
+        console.log(`Error: min (${min}) and max (${max}) differ by less than Number.EPSILON (${Number.EPSILON})`);
+        return [];
+    }
+
+    for (let i=0;i<len;i++) {
+        list.push(min + parseInt(Math.random()*range));
+    }
+    return list;
+}
 // using splice seriously hurts performance
 function genPairedArrayWithMarker(len,min,max,marker) {
         len = len?len:10;
@@ -29,4 +48,54 @@ function genPairedArrayWithMarker(len,min,max,marker) {
     }
     pArray.pop(); //remove removeWhenDone
     return pArray;
+}
+
+function genIntegerList2(len,min,max) {
+    len = len?len:10;
+    min = min?min:0;
+    max = max?max:2**30;
+
+    let range = max-min+1,                  //Note: Math.random() never returns max, so add one;
+        list  = new Array(len).fill(min-1);
+
+    for (let i=0;i<len;i++) {
+        list[i] = (min + parseInt(Math.random()*range));
+    }
+    return list;
+}
+
+
+// using splice seriously hurts performance
+
+function genPairedArrayWithMarker2 (len,min,max,marker) {
+        len = len?len:10;
+        min = min?min:0;
+        max = max?max:2**30;
+        marker = marker?marker:(min + parseInt(Math.random*(max-min)));
+
+    let removeWhenDone = "removeWhenDone",
+        halfLen  = parseInt(Math.floor(len/2)),
+        randList = genIntegerList2(halfLen,min,max),
+        pArray = [randList,marker,new Array(halfLen).fill(min-1),removeWhenDone].flat(),
+        item,
+        returnObj = {randList:randList,marker:marker,pArray:null}
+
+    // mix in marker with randList:
+    let markerIdx = randList.length,
+        swapIdx = parseInt(markerIdx*Math.random());
+    item = pArray[markerIdx];
+    pArray[markerIdx] = pArray[swapIdx];
+    pArray[swapIdx] = item;
+ 
+
+    for (let i=0,j=halfLen+1;i<halfLen;i++,j++) { 
+        item = randList[i];
+        swapIdx = parseInt(j*Math.random());
+        pArray[j] = 0+pArray[swapIdx];
+        pArray[swapIdx] = 0+item;
+    }
+    returnObj.pArray = pArray;
+    pArray.pop(); //remove removeWhenDone
+    //return pArray;
+    return returnObj;
 }
