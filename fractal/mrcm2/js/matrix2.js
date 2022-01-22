ed00110a6bee71a74c2ebfdd7802090261a6e263
Russell Sorensen <russell-github-commits@highfivediet.com>
2022-01-21 22:01:41 -0800

    Final version for codepen fractal/chaos game etc

--- fractal/mrcm2/js/matrix2.js
+++ fractal/mrcm2/js/matrix2.js
@@ -331,3 +331,11 @@ CM.push( // Matrix ..: Sierpinski Carpet 5
 );
 //{a:0,b:1,c:-1,d:0,e:1,f:0,pct:1.0000}
 //{a:1/3,b:0,c:0,d:1/3,e:0,f:0,pct:1/8},
+
+CM.push(
+    new MRCM(600,600,CM.length,`Matrix ${CM.length}: Fractal Line?`,
+        {a:.5,b:0,c:0,d:.5,e:0,f:.5,pct:1/3},
+        {a:.5,b:0,c:0,d:.5,e:1,f:0,pct:1/3},
+        {a:.1,b:0,c:0,d:.1,e:1/2,f:1/2,pct:1/3},
+    )
+);
